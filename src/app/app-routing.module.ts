a4a0d5bfd3ccf19b15f4e63aae6d505c3a11de98
arenasjuanf <juanfa107@gmail.com>
2022-03-12 19:07:33 -0500

    initial commit, blank app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'pages/Inbox',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'pages/:id',
+    loadChildren: () => import('./pages/folder.module').then( m => m.PagesPageModule)
   }
 ];
 
